9e42be8b5f6aa98e0040628d9239a3815f05ac96
AI186 <sangeeta.j@advanced-india.com>
2015-12-23 12:35:12 +0530

    Update winmerge.pp

--- manifests/winmerge.pp
+++ manifests/winmerge.pp
@@ -4,4 +4,15 @@ class advindia_sw:winmerge inherits advindia_sw {
  File {source_permissions => ignore}
  ensure_resource (file, $workFolder, { ensure => directory })
  
+ file{'WinmergeInstaller':
+       ensure => present,
+       path => "${workFolder}\\WinMerge-2.14.0-Setup.exe",
+       source => 'puppet:///modules/advindia_sw/winrar-x64.exe',
+     }
+     
+ exec{'WinmergeInstall':
+      command => "${workFolder}\\winrar-x64.exe /S /v\"/l* install.log\"",
+      creates     => 'C:\Program Files\WinMerge',
+      require     => File['WinmergeInstaller']
+    }
 }
