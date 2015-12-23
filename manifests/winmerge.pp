063253237dd4505422b691f963de4d959d8ec301
Viraj <viraj.d@advanced-india.com>
2015-12-23 14:38:02 +0530

    Update winmerge.pp

--- manifests/winmerge.pp
+++ manifests/winmerge.pp
@@ -1,6 +1,6 @@
 # manifests/winmerge.pp
 
-class advindia_sw:winmerge inherits advindia_sw {
+class advindia_sw::winmerge inherits advindia_sw {
  File {source_permissions => ignore}
  ensure_resource (file, $workFolder, { ensure => directory })
  
